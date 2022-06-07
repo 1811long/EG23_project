cd6ffa488691704e57515bb60c52ef1bd3947da0
tranlevulong <vulongtranle@gmail.com>
2022-06-07 22:09:47 +0200

    updated

--- src/components/CustomizedButton.java
+++ src/components/CustomizedButton.java
@@ -1,10 +1,36 @@
 package components;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 
 public class CustomizedButton extends JButton{
+    public final Font FONT_DEFAULT = new Font("Purisa", Font.BOLD, 13);
+    public final Color BACKGROUND_COLOR = new Color(164,200,225);
     public CustomizedButton(String text){
+        setFont(FONT_DEFAULT);
+        setBorder(new RoundBtn(15));
         setText(text);
+        setBackground(BACKGROUND_COLOR);
         setSize(200,60);
+    }
+}
+
+
+class RoundBtn implements Border
+{
+    private int r;
+    RoundBtn(int r) {
+        this.r = r;
+    }
+    public Insets getBorderInsets(Component c) {
+        return new Insets(this.r+1, this.r+1, this.r+2, this.r);
+    }
+    public boolean isBorderOpaque() {
+        return true;
+    }
+    public void paintBorder(Component c, Graphics g, int x, int y,
+                            int width, int height) {
+        g.drawRoundRect(x, y, width-1, height-1, r, r);
     }
 }
--- src/page_accueil.java
+++ src/page_accueil.java
@@ -3,10 +3,16 @@ import components.CustomizedButton;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class page_accueil {
 
 	private JFrame frame;
+
+	private CustomizedButton jouerButton = new CustomizedButton("JOUER");
+	private CustomizedButton parametrageButton = new CustomizedButton("PARAMETRAGE");
+	private CustomizedButton aideButton = new CustomizedButton("AIDE");
 
 	/**
 	 * Launch the application.
@@ -40,8 +46,14 @@ public class page_accueil {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
-		CustomizedButton startButton = new CustomizedButton("start");
-		startButton.setLocation(200,200);
+		jouerButton.setLocation(143,140);
+		parametrageButton.setLocation(143,238);
+		aideButton.setLocation(143,349);
+
+		frame.getContentPane().add(jouerButton);
+		frame.getContentPane().add(parametrageButton);
+		frame.getContentPane().add(aideButton);
+
 	}
 
 }
